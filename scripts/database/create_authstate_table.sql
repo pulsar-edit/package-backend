34b0253ccfc6d628f0f0baa17dd5ec8d7dbe969f
Digitalone <kurmikon@libero.it>
2023-01-06 12:56:36 +0100

    authstate: add cleanup delete query

--- scripts/database/create_authstate_table.sql
+++ scripts/database/create_authstate_table.sql
@@ -3,3 +3,8 @@ CREATE TABLE authstate (
     keycode VARCHAR(256) NOT NULL UNIQUE,
     created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Query for the cleanup of the unused keys to be executed
+-- regularly on the server by a cron job service.
+DELETE FROM authstate
+WHERE CURRENT_TIMESTAMP > created + INTERVAL '2 minutes';
