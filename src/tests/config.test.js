f2339a675aaa7c122b28591964a00b44a39b193a
confused-Techie <dev@lhbasics.com>
2023-02-08 19:12:54 -0800

    Allow empty strings in config

--- src/tests/config.test.js
+++ src/tests/config.test.js
@@ -11,24 +11,24 @@ describe("Config Returns all Expected Values", () => {
 
     const schema = Joi.object().keys({
       port: Joi.number().integer().required(),
-      server_url: Joi.string().required(),
+      server_url: Joi.string().allow("").required(),
       paginated_amount: Joi.number().integer().required(),
       prod: Joi.boolean().required(),
       cache_time: Joi.number().integer().required(),
-      GCLOUD_STORAGE_BUCKET: Joi.string().required(),
-      GOOGLE_APPLICATION_CREDENTIALS: Joi.string().required(),
-      GH_CLIENTID: Joi.string().required(),
-      GH_USERAGENT: Joi.string().required(),
-      GH_REDIRECTURI: Joi.string().required(),
-      GH_CLIENTSECRET: Joi.string().required(),
-      DB_HOST: Joi.string().required(),
-      DB_USER: Joi.string().required(),
-      DB_PASS: Joi.string().required(),
-      DB_DB: Joi.string().required(),
+      GCLOUD_STORAGE_BUCKET: Joi.string().allow("").required(),
+      GOOGLE_APPLICATION_CREDENTIALS: Joi.string().allow("").required(),
+      GH_CLIENTID: Joi.string().allow("").required(),
+      GH_USERAGENT: Joi.string().allow("").required(),
+      GH_REDIRECTURI: Joi.string().allow("").required(),
+      GH_CLIENTSECRET: Joi.string().allow("").required(),
+      DB_HOST: Joi.string().allow("").required(),
+      DB_USER: Joi.string().allow("").required(),
+      DB_PASS: Joi.string().allow("").required(),
+      DB_DB: Joi.string().allow("").required(),
       DB_PORT: Joi.number().integer().required(),
-      DB_SSL_CERT: Joi.string().required(),
+      DB_SSL_CERT: Joi.string().allow("").required(),
       LOG_LEVEL: Joi.number().integer().required(),
-      LOG_FORMAT: Joi.string().required()
+      LOG_FORMAT: Joi.string().allow("").required()
     }).required();
 
     expect(con).toMatchSchema(schema);
