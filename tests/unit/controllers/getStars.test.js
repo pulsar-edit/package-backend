701d0e3c4a714db9b0cb73cc1a685fff5abab08c
confused-Techie <dev@lhbasics.com>
2023-09-15 00:43:41 -0700

    100% coverage in `getStars.js`

--- tests/unit/controllers/getStars.test.js
+++ tests/unit/controllers/getStars.test.js
@@ -27,4 +27,71 @@ describe("Returns as expected", () => {
     expect(res.content).toBeDefined();
     expect(res.content.content).toBe("Test Failure");
   });
+
+  test("When 'db.getStarredPointersByUserID' fails", async () => {
+    const localContext = context;
+    localContext.auth = {
+      verifyAuth: () => { return { ok: true, content: { id: 1 } }; }
+    };
+    localContext.database = {
+      getStarredPointersByUserID: () => { return { ok: false, content: "db Test Failure" }; }
+    };
+
+    const res = await endpoint.logic({}, localContext);
+
+    expect(res.ok).toBe(false);
+    expect(res.content).toBeDefined();
+    expect(res.content.content).toBe("db Test Failure");
+  });
+
+  test("When the user has no stars", async () => {
+    const localContext = context;
+    localContext.auth = {
+      verifyAuth: () => { return { ok: true, content: { id: 1 } }; }
+    };
+    localContext.database = {
+      getStarredPointersByUserID: () => { return { ok: true, content: [] }; }
+    };
+
+    const res = await endpoint.logic({}, localContext);
+
+    expect(res.ok).toBe(true);
+    expect(res.content).toBeArray();
+    expect(res.content.length).toBe(0);
+  });
+
+  test("When 'db.getPackageCollectionByID' fails", async () => {
+    const localContext = context;
+    localContext.auth = {
+      verifyAuth: () => { return { ok: true, content: { id: 1 } }; }
+    };
+    localContext.database = {
+      getStarredPointersByUserID: () => { return { ok: true, content: [ "an_id" ] }; },
+      getPackageCollectionByID: () => { return { ok: false, content: "Another DB Error" }; }
+    };
+
+    const res = await endpoint.logic({}, localContext);
+
+    expect(res.ok).toBe(false);
+    expect(res.content.content).toBe("Another DB Error");
+  });
+
+  test("When request succeeds", async () => {
+    const localContext = context;
+    localContext.auth = {
+      verifyAuth: () => { return { ok: true, content: { id: 1 } }; }
+    };
+    localContext.database = {
+      getStarredPointersByUserID: () => { return { ok: true, content: [ "an_id" ] }; },
+      getPackageCollectionByID: () => { return { ok: true, content: {} }; }
+    };
+    localContext.utils = {
+      constructPackageObjectShort: () => { return { item: "is_a_package" }; }
+    };
+
+    const res = await endpoint.logic({}, localContext);
+
+    expect(res.ok).toBe(true);
+    expect(res.content.item).toBe("is_a_package");
+  });
 });
