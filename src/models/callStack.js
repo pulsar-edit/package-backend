19415e3cb7f2ce352a0235031c088f143a155971
confused-Techie <dev@lhbasics.com>
2024-02-19 16:48:28 -0800

    Add support for every type of value, and fully test

--- src/models/callStack.js
+++ src/models/callStack.js
@@ -20,21 +20,65 @@ module.exports = class CallStack {
 
   // Attempts to remove any sensitive data that may be found within
   sanitize(content) {
-    if (typeof content !== "object") {
-      return content;
+
+    const hideString = "*****";
+    let outContent = {};
+    let type = typeof content;
+
+    // Since JavaScript `typeof` will assign an array as "object" as well as null
+    // we will extend this typeof check to add those as different types, to ease
+    // the complexity of the below switch statement
+    if (Array.isArray(content)) {
+      type = "array";
+    }
+    if (content === null) {
+      type = "null";
     }
 
-    let outContent = {};
+    switch(type) {
+      case "object":
+        for (const key in content) {
+          // Match different possible keys that represent sensitive data
+          switch(key) {
+            case "token":
+              outContent[key] = hideString;
+              break;
+            default:
+              outContent[key] = this.sanitize(content[key]);
+              break;
+          }
+        }
+        break;
+      case "string":
+        // Match different strings of sensitive data
 
-    for (const key in content) {
-      switch (key) {
-        case "token":
-          outContent[key] = "*****";
-          break;
-        default:
-          outContent[key] = content[key];
-          break;
-      }
+        // https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/about-authentication-to-github#githubs-token-formats
+        if (content.startsWith("gho_")) {
+          outContent = hideString;
+        } else if (content.startsWith("ghp_")) {
+          outContent = hideString;
+        } else if (content.startsWith("github_pat_")) {
+          outContent = hideString;
+        } else if (content.startsWith("ghu_")) {
+          outContent = hideString;
+        } else if (content.startsWith("ghs_")) {
+          outContent = hideString;
+        } else if (content.startsWith("ghr_")) {
+          outContent = hideString;
+        } else {
+          // String seems safe
+          outContent = content;
+        }
+        break;
+      case "array":
+        let tmpArr = [];
+        for (let i = 0; i < content.length; i++) {
+          tmpArr.push(this.sanitize(content[i]));
+        }
+        outContent = tmpArr;
+        break;
+      default:
+        outContent = content;
     }
 
     return outContent;
--- tests/unit/models/callStack.test.js
+++ tests/unit/models/callStack.test.js
@@ -1,4 +1,5 @@
 const callStack = require("../../../src/models/callStack.js");
+const hideValue = "*****";
 
 describe("Sanitizes content as expected", () => {
   test("Leaves safe data unmodified", () => {
@@ -22,6 +23,60 @@ describe("Sanitizes content as expected", () => {
 
     const after = cs.sanitize(before);
 
-    expect(after).toEqual({ token: "*****" });
+    expect(after).toEqual({ token: hideValue });
+  });
+
+  test("Removes value of an unsafe string", () => {
+    const cs = new callStack();
+
+    const before = "gho_value";
+
+    const after = cs.sanitize(before);
+
+    expect(after).toEqual(hideValue);
+  });
+
+  test("Removes deeply nested unsafe string", () => {
+    const cs = new callStack();
+
+    const before = { value: { value: { value: { value: "github_pat_value" }}}};
+
+    const after = cs.sanitize(before);
+
+    expect(after).toEqual({ value: { value: { value: { value: hideValue }}}});
+  });
+
+  test("Removes unsafe value from array", () => {
+    const cs = new callStack();
+
+    const before = [{}, {}, { token: "super_secret" }];
+    const after = cs.sanitize(before);
+
+    expect(after).toEqual([{}, {}, { token: hideValue }]);
+  });
+
+  test("Doesn't break on 'null'", () => {
+    const cs = new callStack();
+
+    const before = null;
+    const after = cs.sanitize(before);
+
+    expect(after).toEqual(null);
+  });
+
+  test("Removes all known github token formats", () => {
+    const cs = new callStack();
+
+    const before = [
+      { v1: "ghp_personal_access_token_classic" },
+      { v2: "github_pat_fine_grained_personal_access_token" },
+      { v3: "gho_oauth_access_token" },
+      { v4: "ghu_user_access_token_for_github_app" },
+      { v5: "ghs_installation_access_token" },
+      { v6: "ghr_refresh_token_for_github_app" }
+    ];
+    const after = cs.sanitize(before);
+
+    expect(after).toEqual([{v1: hideValue}, {v2: hideValue}, {v3: hideValue}, {v4: hideValue}, {v5: hideValue}, {v6: hideValue}]);
   });
 });
