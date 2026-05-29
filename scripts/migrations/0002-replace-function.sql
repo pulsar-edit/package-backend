dfb22ff3d5922c8783e393817074feb0c026c5d3
confused-Techie <dev@lhbasics.com>
2026-05-29 16:59:01 -0700

    Exclude the `versions` table from `now_on_updated_package` check

--- scripts/migrations/0002-replace-function.sql
+++ scripts/migrations/0002-replace-function.sql
@@ -1,6 +1,12 @@
 CREATE OR REPLACE FUNCTION now_on_updated_package()
 RETURNS TRIGGER AS $$
 BEGIN
+  IF TG_TABLE_NAME = 'versions' THEN
+    -- Since this function is also triggerred on the version table changes
+    -- and the version table does not contain a downloads column, we exclude
+    -- the check when operating on the versions table.
+    RETURN NEW;
+  END IF;
   IF (OLD.downloads = NEW.downloads) THEN
     -- Only change the updated column when the update is not triggerred
     -- by changing the download count.
