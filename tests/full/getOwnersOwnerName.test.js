38981408cbd3dcfec2bd3c4484b52a9af6da20af
confused-Techie <dev@lhbasics.com>
2026-06-13 20:15:46 -0700

    Fix bug in `/api/owners/:ownerName` that wouldn't apply owner clause in DB query

--- tests/full/getOwnersOwnerName.test.js
+++ tests/full/getOwnersOwnerName.test.js
@@ -20,12 +20,23 @@ describe("GET /api/owners/:ownerName", () => {
   });
 
   test("Returns an empty result if the owner doesn't exist", async () => {
+    // == Setup
+    const addPkg = await database.insertNewPackage(
+      genPackage("https://github.com/pulsar-cooperative/get-owner-test")
+    );
+    expect(addPkg.ok).toBe(true);
+
+    // == Test
     const res = await supertest(app)
       .get("/api/owners/i-dont-exist");
 
     expect(res).toHaveHTTPCode(200);
     expect(res.body).toBeArray();
     expect(res.body.length).toBe(0);
+
+    // == Cleanup
+    const removePkg = await database.removePackageByName("get-owner-test", true);
+    expect(removePkg.ok).toBe(true);
   });
 
   test("Returns a package owned by the owner", async () => {
