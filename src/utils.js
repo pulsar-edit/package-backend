311db8f4780381bc7e2453dbad37f82b49385d85
confused-Techie <dev@lhbasics.com>
2026-06-15 18:58:31 -0700

    Use `semver` package instead of custom logic in `utils.engineFilter`

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,7 @@
 const logger = require("./logger.js");
 const storage = require("./storage.js");
 const crypto = require("crypto");
+const semver = require("semver");
 
 /**
  * @async
@@ -34,12 +35,11 @@ async function isPackageNameBanned(name) {
 /**
  * @async
  * @function engineFilter
- * @desc A complex function that provides filtering by Atom engine version.
+ * @desc A function that provides filtering by Atom engine version.
  * This should take a package with its versions and retrieve whatever matches
  * that engine version as provided.
  * @returns {object} The filtered object.
  */
-// eslint-disable-next-line complexity
 async function engineFilter(pack, engine) {
   // If a compatible version is found, we add its data to the metadata property of the package
   // Otherwise we return an unmodified package, so that it is usable to the consumer.
@@ -53,13 +53,13 @@ async function engineFilter(pack, engine) {
     return pack;
   }
 
-  const engSv = semverArray(engine);
+  const engineValid = semver.valid(engine);
 
   // Validate engine semver format.
-  if (engSv === null) {
+  if (engineValid === null) {
     logger.generic(5, "engineFilter returning non-valid Engine semverArray", {
       type: "object",
-      obj: engSv,
+      obj: engineValid,
     });
     return pack;
   }
@@ -74,120 +74,16 @@ async function engineFilter(pack, engine) {
       continue;
     }
 
-    // Core Atom Packages contain '*' as the engine type, and will require a
-    // manual check.
-    if (pack.versions[ver].engines.atom === "*") {
-      break;
-    }
+    const rangeValid = semver.validRange(pack.versions[ver].engines.atom);
 
-    // Track the upper and lower end conditions.
-    // Null type means not available; Bool type means available with the
-    // relative result.
-    let lowerEnd = null;
-    let upperEnd = null;
-
-    // Extract the lower end semver condition (i.e >=1.0.0)
-    const lowSv = pack.versions[ver].engines.atom.match(
-      /(>=?)(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/
-    );
-
-    if (lowSv !== null) {
-      // Lower end condition present, so test it.
-      switch (lowSv[0]) {
-        case ">":
-          lowerEnd = semverGt(
-            [engSv[0], engSv[1], engSv[2]],
-            [lowSv[2], lowSv[3], lowSv[4]]
-          );
-
-          break;
-        case ">=":
-          lowerEnd =
-            semverGt(
-              [engSv[0], engSv[1], engSv[2]],
-              [lowSv[2], lowSv[3], lowSv[4]]
-            ) ||
-            semverEq(
-              [engSv[0], engSv[1], engSv[2]],
-              [lowSv[2], lowSv[3], lowSv[4]]
-            );
-
-          break;
-      }
-    }
-
-    // Extract the upper end semver condition (i.e <=2.0.0)
-    const upSv = pack.versions[ver].engines.atom.match(
-      /(<=?)(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/
-    );
-
-    if (upSv !== null) {
-      // Upper end condition present, so test it.
-      switch (upSv[1]) {
-        case "<":
-          upperEnd = semverLt(
-            [engSv[0], engSv[1], engSv[2]],
-            [upSv[2], upSv[3], upSv[4]]
-          );
-
-          break;
-        case "<=":
-          upperEnd =
-            semverLt(
-              [engSv[0], engSv[1], engSv[2]],
-              [upSv[2], upSv[3], upSv[4]]
-            ) ||
-            semverEq(
-              [engSv[0], engSv[1], engSv[2]],
-              [upSv[2], upSv[3], upSv[4]]
-            );
-
-          break;
-      }
-    }
-
-    if (lowerEnd === null && upperEnd === null) {
-      // Both lower and upper end condition are unavailable.
-      // So, as last resort, check if there is an equality condition (i.e =1.0.0)
-      const eqSv = pack.versions[ver].engines.atom.match(
-        /^=(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$/
-      );
-
-      if (
-        eqSv !== null &&
-        semverEq([engSv[0], engSv[1], engSv[2]], [eqSv[1], eqSv[2], eqSv[3]])
-      ) {
-        compatibleVersion = ver;
-
-        break; // Found the compatible version, break the loop.
-      }
-
-      // Equality condition unavailable or not satisfied, skip the current loop.
+    if (!rangeValid) {
+      // If it's not a valid range, skip the current loop
       continue;
     }
 
-    // One of the semver condition may still be not present.
-    if (lowerEnd === null) {
-      // Only upper end available
-      if (upperEnd) {
-        compatibleVersion = ver;
-
-        break; // The version is under the upper end, break the loop.
-      }
-    } else if (upperEnd === null) {
-      // Only lower end available
-      if (lowerEnd) {
-        compatibleVersion = ver;
-
-        break; // The version is over the lower end, break the loop.
-      }
-    }
-
-    // Both lower and upper end are available.
-    if (lowerEnd && upperEnd) {
+    if (semver.satisfies(engineValid, rangeValid)) {
       compatibleVersion = ver;
-
-      break; // The version is within the range, break the loop.
+      break;
     }
   }
 
@@ -201,85 +97,6 @@ async function engineFilter(pack, engine) {
   pack.metadata = pack.versions[compatibleVersion];
 
   return pack;
-}
-
-/**
- * @function semverArray
- * @desc Takes a semver string and returns it as an Array of strings.
- * This can also be used to check for semver valitidy. If it's not a semver, null is returned.
- * @param {string} semver
- * @returns {array|null} The formatted semver in array of three strings, or null if no match.
- * @example <caption>Valid Semver Passed</caption>
- * // returns ["1", "2", "3" ]
- * semverArray("1.2.3");
- * @example <caption>Invalid Semver Passed</caption>
- * // returns null
- * semverArray("1.Hello.World");
- */
-function semverArray(semver) {
-  let array =
-    typeof semver === "string"
-      ? semver.match(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/) ?? []
-      : [];
-
-  // Returning null on no match or slice the array to get only indexes from 1 to 3.
-  return array.length !== 4 ? null : array.slice(1);
-}
-
-/**
- * @function semverGt
- * @desc Compares two sermver and return true if the first is greater than the second.
- * Expects to get the semver formatted as array of strings.
- * Should be always executed after running semverArray.
- * @param {array} a1 - First semver as array of strings.
- * @param {array} a2 - Second semver as array of string.
- * @returns {boolean} The result of the comparison
- */
-function semverGt(a1, a2) {
-  const v1 = a1.map((n) => parseInt(n, 10));
-  const v2 = a2.map((n) => parseInt(n, 10));
-
-  if (v1[0] > v2[0]) {
-    return true;
-  } else if (v1[0] < v2[0]) {
-    return false;
-  }
-
-  if (v1[1] > v2[1]) {
-    return true;
-  } else if (v1[1] < v2[1]) {
-    return false;
-  }
-
-  return v1[2] > v2[2];
-}
-
-/**
- * @function semverLt
- * @desc Compares two sermver and return true if the first is less than the second.
- * Expects to get the semver formatted as array of strings.
- * Should be always executed after running semverArray.
- * @param {array} a1 - First semver as array of strings.
- * @param {array} a2 - Second semver as array of strings.
- * @returns {boolean} The result of the comparison
- */
-function semverLt(a1, a2) {
-  const v1 = a1.map((n) => parseInt(n, 10));
-  const v2 = a2.map((n) => parseInt(n, 10));
-
-  if (v1[0] < v2[0]) {
-    return true;
-  } else if (v1[0] > v2[0]) {
-    return false;
-  }
-
-  if (v1[1] < v2[1]) {
-    return true;
-  } else if (v1[1] > v2[1]) {
-    return false;
-  }
-
-  return v1[2] < v2[2];
 }
 
 /**
@@ -336,19 +153,6 @@ function getOwnerRepoFromUrlString(url) {
 }
 
 /**
- * @function semverEq
- * @desc Compares two sermver and return true if the first is equal to the second.
- * Expects to get the semver formatted as array of strings.
- * Should be always executed after running semverArray.
- * @param {array} a1 - First semver as array.
- * @param {array} a2 - Second semver as array.
- * @returns {boolean} The result of the comparison.
- */
-function semverEq(a1, a2) {
-  return a1[0] === a2[0] && a1[1] === a2[1] && a1[2] === a2[2];
-}
-
-/**
  * @function generateRandomString
  * @desc Uses the crypto module to generate and return a random string.
  * @param {string} n - The number of bytes to generate.
@@ -362,10 +166,6 @@ function generateRandomString(n) {
 module.exports = {
   isPackageNameBanned,
   engineFilter,
-  semverArray,
-  semverGt,
-  semverLt,
-  semverEq,
   getOwnerRepoFromPackage,
   getOwnerRepoFromUrlString,
   generateRandomString,
