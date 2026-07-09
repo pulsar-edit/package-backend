ab4ce9de8977b066d7620b9ec2cfc2bd1fcb1d1d
confused-Techie <dev@lhbasics.com>
2026-07-08 18:21:39 -0700

    Ensure original comment appears in new file

--- src/controllers/packagesPackageNameVersionsVerionNameEventsUninstall.js
+++ src/controllers/packagesPackageNameVersionsVerionNameEventsUninstall.js
@@ -39,7 +39,22 @@ module.exports = {
           preferResponse: 201,
         },
         middleware: ["$DEFAULT"],
-        func: async (ctx, next) => { ctx.status = 201; }
+        func: async (ctx, next) => {
+          /**
+            Used when a package is uninstalled, decreases the download count by 1.
+            Originally an undocumented endpoint.
+            The decision to return a '201' is based on how other POST endpoints return,
+            during a successful event.
+            This endpoint has now been deprecated, as it serves no useful features,
+            and on further examination may have been intended as a way to collect
+            data on users, which is not something we implement.
+            * Deprecated since v1.0.2
+            * see: https://github.com/atom/apm/blob/master/src/uninstall.coffee
+            * While decoupling HTTP handling from logic, the function has been removed
+              entirely: https://github.com/pulsar-edit/package-backend/pull/171
+          */
+          ctx.status = 201;
+        }
       }
     }
   }
